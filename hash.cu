f1762de8ee8c5082e0fc7dd7ad09c3ee8cff0924
AurelienMoisson <44036815+AurelienMoisson@users.noreply.github.com>
2019-03-29 15:20:00 +0100

    changed data structure

--- hash.cu
+++ hash.cu
@@ -3,7 +3,19 @@
 
 #define ROTR(x,n) (((x) >> (n)) | ((x) << ((32) - (n))))
 
-void sha256(int N, char *array, uint32_t *k, uint32_t *w, uint32_t *cst_h)
+__constant__ uint32_t cst_k[] = {
+   0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+__device__
+void sha256(int N, char *array, uint32_t *w, uint32_t *h_result)
 {
 	/* N is size of array
 	   array is preprocessed data to hash
@@ -12,14 +24,14 @@ void sha256(int N, char *array, uint32_t *k, uint32_t *w, uint32_t *cst_h)
 	   		it will be written over. This prevents mallocs in hash function.
 	   h is an 8 entry array where the hash result will be put
 	*/
-	cst_h[0] = 0x6a09e667;
-	cst_h[1] = 0xbb67ae85;
-	cst_h[2] = 0x3c6ef372;
-	cst_h[3] = 0xa54ff53a;
-	cst_h[4] = 0x510e527f;
-	cst_h[5] = 0x9b05688c;
-	cst_h[6] = 0x1f83d9ab;
-	cst_h[7] = 0x5be0cd19;
+	h_result[0] = 0x6a09e667;
+    h_result[0] = 0xbb67ae85;
+    h_result[0] = 0x3c6ef372;
+    h_result[0] = 0xa54ff53a;
+    h_result[0] = 0x510e527f;
+    h_result[0] = 0x9b05688c;
+    h_result[0] = 0x1f83d9ab;
+    h_result[0] = 0x5be0cd19;
 
 	/* pass through array in 512bits chunks (64 bytes) */
 	int N_chunks = N>>6;
@@ -37,19 +49,19 @@ void sha256(int N, char *array, uint32_t *k, uint32_t *w, uint32_t *cst_h)
 			w[i] = w[i-16] + s0 + w[i-7] + s1;
 		}
 
-		uint32_t a = cst_h[0];
-		uint32_t b = cst_h[1];
-		uint32_t c = cst_h[2];
-		uint32_t d = cst_h[3];
-		uint32_t e = cst_h[4];
-		uint32_t f = cst_h[5];
-		uint32_t g = cst_h[6];
-		uint32_t h = cst_h[7];
+		uint32_t a = h_result[0];
+		uint32_t b = h_result[1];
+		uint32_t c = h_result[2];
+		uint32_t d = h_result[3];
+		uint32_t e = h_result[4];
+		uint32_t f = h_result[5];
+		uint32_t g = h_result[6];
+		uint32_t h = h_result[7];
 
 		for (int i = 0; i < 64; i++){
 			uint32_t S1 = ROTR(e, 6) xor ROTR(e, 11) xor ROTR(e, 25);
 			uint32_t ch = (e and f) xor ((not e) and g);
-			uint32_t temp1 = h + S1 + ch + k[i] + w[i];
+			uint32_t temp1 = h + S1 + ch + cst_k[i] + w[i];
 			uint32_t S0 = ROTR(a, 2) xor ROTR(a, 13) xor ROTR(a, 22);
 			uint32_t maj = (a and b) xor (a and c) xor (b and c);
 			uint32_t temp2 = S0 + maj;
@@ -63,13 +75,14 @@ void sha256(int N, char *array, uint32_t *k, uint32_t *w, uint32_t *cst_h)
 			b = a;
 			a = temp1 + temp2;
 		}
-		cst_h[0] += a;
-		cst_h[1] += b;
-		cst_h[2] += c;
-		cst_h[3] += d;
-		cst_h[4] += e;
-		cst_h[5] += f;
-		cst_h[6] += g;
-		cst_h[7] += h;
+		h_result[0] += a;
+		h_result[1] += b;
+		h_result[2] += c;
+		h_result[3] += d;
+		h_result[4] += e;
+		h_result[5] += f;
+		h_result[6] += g;
+		h_result[7] += h;
 	}
 }
+
