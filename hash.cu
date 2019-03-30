9571265c22f4bf02bee76cf63f043149c73804d9
AurelienMoisson <44036815+AurelienMoisson@users.noreply.github.com>
2019-03-30 16:40:51 +0100

    fixed indentation

--- hash.cu
+++ hash.cu
@@ -17,14 +17,14 @@ __constant__ uint32_t cst_k[] = {
 __device__
 void sha256(int N, char *array, uint32_t *w, uint32_t *h_result, int nonce_position, uint64_t nonce_value)
 {
-	/* N is size of array
-	   array is preprocessed data to hash
-	   k is the 64 variables constants to use in sha256
-	   w is a 64 entry message schedule array; it can contain anything,
-	   		it will be written over. This prevents mallocs in hash function.
-	   h is an 8 entry array where the hash result will be put
-	*/
-	h_result[0] = 0x6a09e667;
+    /* N is size of array
+       array is preprocessed data to hash
+       k is the 64 variables constants to use in sha256
+       w is a 64 entry message schedule array; it can contain anything,
+               it will be written over. This prevents mallocs in hash function.
+       h is an 8 entry array where the hash result will be put
+    */
+    h_result[0] = 0x6a09e667;
     h_result[0] = 0xbb67ae85;
     h_result[0] = 0x3c6ef372;
     h_result[0] = 0xa54ff53a;
@@ -33,141 +33,141 @@ void sha256(int N, char *array, uint32_t *w, uint32_t *h_result, int nonce_posit
     h_result[0] = 0x1f83d9ab;
     h_result[0] = 0x5be0cd19;
 
-	/* pass through array in 512bits chunks (64 bytes) */
-	int N_chunks = N>>6;
-	for (int chunk_i = 0; chunk_i < N_chunks; chunk_i++){
-		int chunk_start = chunk_i<<6;
-		
-		/* copy chunk in first 8 values of w */
-		memcpy(w, &array[chunk_start], 64);
-		
-		// TODO : review this chunk of code
-		// calculate pos relative to chunk_start in terms of bytes
-		int pos_in_w = nonce_position - chunk_start;
-		//change nonce if it is in the chunk
-		if (0 <= pos_in_w <= 64){
-			if (pos_in_w <= 56){
-				/* if the nonce is completely in the chunk,
-				   copy it completely at the right location
-				   (the syntax is weird because nonce_position is in bytes
-				    while w is uint32_t*) */
-				*(w + pos_in_w) = nonce_value;
-			}else {
-				// copy only a left part of the nonce
-				memcpy(w + pos_in_w,
-					   &nonce_value,
-					   64-pos_in_w);
-			}
-		}
-		if (-8 < pos_in_w < 0){
-			// copy only a right part of the nonce
-			memcpy(w + pos_in_w,
-				   &nonce_value + 8 - pos_in_w,
-				   8 - pos_in_w);
-		}
+    /* pass through array in 512bits chunks (64 bytes) */
+    int N_chunks = N>>6;
+    for (int chunk_i = 0; chunk_i < N_chunks; chunk_i++){
+        int chunk_start = chunk_i<<6;
+        
+        /* copy chunk in first 8 values of w */
+        memcpy(w, &array[chunk_start], 64);
+        
+        // TODO : review this chunk of code
+        // calculate pos relative to chunk_start in terms of bytes
+        int pos_in_w = nonce_position - chunk_start;
+        //change nonce if it is in the chunk
+        if (0 <= pos_in_w <= 64){
+            if (pos_in_w <= 56){
+                /* if the nonce is completely in the chunk,
+                   copy it completely at the right location
+                   (the syntax is weird because nonce_position is in bytes
+                    while w is uint32_t*) */
+                *(w + pos_in_w) = nonce_value;
+            }else {
+                // copy only a left part of the nonce
+                memcpy(w + pos_in_w,
+                       &nonce_value,
+                       64-pos_in_w);
+            }
+        }
+        if (-8 < pos_in_w < 0){
+            // copy only a right part of the nonce
+            memcpy(w + pos_in_w,
+                   &nonce_value + 8 - pos_in_w,
+                   8 - pos_in_w);
+        }
 
-		/* complete w by following some weird rules */
-		for (int i = 16; i < 64; i++){
-			uint32_t w15 = w[i-15];
-			uint32_t s0 = ROTR(w15, 7) xor ROTR(w15, 18) xor w15 << 3;
-			uint32_t w2 = w[i-2];
-			uint32_t s1 = ROTR(w2, 17) xor ROTR(w2, 19) xor w2 << 10;
-			w[i] = w[i-16] + s0 + w[i-7] + s1;
-		}
+        /* complete w by following some weird rules */
+        for (int i = 16; i < 64; i++){
+            uint32_t w15 = w[i-15];
+            uint32_t s0 = ROTR(w15, 7) xor ROTR(w15, 18) xor w15 << 3;
+            uint32_t w2 = w[i-2];
+            uint32_t s1 = ROTR(w2, 17) xor ROTR(w2, 19) xor w2 << 10;
+            w[i] = w[i-16] + s0 + w[i-7] + s1;
+        }
 
-		uint32_t a = h_result[0];
-		uint32_t b = h_result[1];
-		uint32_t c = h_result[2];
-		uint32_t d = h_result[3];
-		uint32_t e = h_result[4];
-		uint32_t f = h_result[5];
-		uint32_t g = h_result[6];
-		uint32_t h = h_result[7];
+        uint32_t a = h_result[0];
+        uint32_t b = h_result[1];
+        uint32_t c = h_result[2];
+        uint32_t d = h_result[3];
+        uint32_t e = h_result[4];
+        uint32_t f = h_result[5];
+        uint32_t g = h_result[6];
+        uint32_t h = h_result[7];
 
-		for (int i = 0; i < 64; i++){
-			uint32_t S1 = ROTR(e, 6) xor ROTR(e, 11) xor ROTR(e, 25);
-			uint32_t ch = (e and f) xor ((not e) and g);
-			uint32_t temp1 = h + S1 + ch + cst_k[i] + w[i];
-			uint32_t S0 = ROTR(a, 2) xor ROTR(a, 13) xor ROTR(a, 22);
-			uint32_t maj = (a and b) xor (a and c) xor (b and c);
-			uint32_t temp2 = S0 + maj;
+        for (int i = 0; i < 64; i++){
+            uint32_t S1 = ROTR(e, 6) xor ROTR(e, 11) xor ROTR(e, 25);
+            uint32_t ch = (e and f) xor ((not e) and g);
+            uint32_t temp1 = h + S1 + ch + cst_k[i] + w[i];
+            uint32_t S0 = ROTR(a, 2) xor ROTR(a, 13) xor ROTR(a, 22);
+            uint32_t maj = (a and b) xor (a and c) xor (b and c);
+            uint32_t temp2 = S0 + maj;
 
-			h = g;
-			g = f;
-			f = e;
-			e = d + temp1;
-			d = c;
-			c = b;
-			b = a;
-			a = temp1 + temp2;
-		}
-		h_result[0] += a;
-		h_result[1] += b;
-		h_result[2] += c;
-		h_result[3] += d;
-		h_result[4] += e;
-		h_result[5] += f;
-		h_result[6] += g;
-		h_result[7] += h;
-	}
+            h = g;
+            g = f;
+            f = e;
+            e = d + temp1;
+            d = c;
+            c = b;
+            b = a;
+            a = temp1 + temp2;
+        }
+        h_result[0] += a;
+        h_result[1] += b;
+        h_result[2] += c;
+        h_result[3] += d;
+        h_result[4] += e;
+        h_result[5] += f;
+        h_result[6] += g;
+        h_result[7] += h;
+    }
 }
 
 
 char *preprocess_sha256(uint64_t length, char *array)
 {
-	// final_length is smallest number over (or equal) to length+1+8 that is divisible by 512
-	uint64_t final_length = ((length + 8)>>9 +1)<<9;
+    // final_length is smallest number over (or equal) to length+1+8 that is divisible by 512
+    uint64_t final_length = ((length + 8)>>9 +1)<<9;
 
-	// allocate space on ram
-	char *host_array;
-	host_array = (char*) malloc(final_length);
-	// initialize final padding at 0
-	for (int i = 1; i <= 64; i += 1){
-		host_array[final_length - i] = 0;
-	}
+    // allocate space on ram
+    char *host_array;
+    host_array = (char*) malloc(final_length);
+    // initialize final padding at 0
+    for (int i = 1; i <= 64; i += 1){
+        host_array[final_length - i] = 0;
+    }
 
-	// allocate memory on gpu
-	char *device_array = 0;
-	cudaMalloc((void**) &device_array, final_length);
+    // allocate memory on gpu
+    char *device_array = 0;
+    cudaMalloc((void**) &device_array, final_length);
 
-	// create processed array
-	// copy message
-	memcpy(host_array, array, length);
-	// put a 1 after the message
-	host_array[length] = '\x80';
-	// write message length at the end
-	char last_char = 0;
-	for (int shift = 54; shift >= 0; shift -= 8){
-		last_char = length>>shift - last_char<<8;
-		host_array[final_length - 8] = last_char;
-	}
+    // create processed array
+    // copy message
+    memcpy(host_array, array, length);
+    // put a 1 after the message
+    host_array[length] = '\x80';
+    // write message length at the end
+    char last_char = 0;
+    for (int shift = 54; shift >= 0; shift -= 8){
+        last_char = length>>shift - last_char<<8;
+        host_array[final_length - 8] = last_char;
+    }
 
 
-	// copy array
-	cudaMemcpy(device_array, array, final_length, cudaMemcpyHostToDevice);
+    // copy array
+    cudaMemcpy(device_array, array, final_length, cudaMemcpyHostToDevice);
 
-	// free host memory space
-	free(host_array);
+    // free host memory space
+    free(host_array);
 
-	return device_array;
+    return device_array;
 }
 
 
 uint32_t *prepare_h_results(int n_of_threads)
 {
-	uint32_t *h_results_pos;
-	// 32 bytes for each tread (256bits)
-	cudaMalloc((void**) &h_results_pos, n_of_threads * 32);
-	return h_results_pos;
+    uint32_t *h_results_pos;
+    // 32 bytes for each tread (256bits)
+    cudaMalloc((void**) &h_results_pos, n_of_threads * 32);
+    return h_results_pos;
 }
 
 
 uint32_t *prepare_working_memories(int n_of_threads)
 {
-	uint32_t *working_memories;
-	// 64 word long working memory for each thread
-	cudaMalloc((void**) &working_memories, n_of_threads * sizeof(uint32_t) * 64);
-	return working_memories;
+    uint32_t *working_memories;
+    // 64 word long working memory for each thread
+    cudaMalloc((void**) &working_memories, n_of_threads * sizeof(uint32_t) * 64);
+    return working_memories;
 }
 
 
